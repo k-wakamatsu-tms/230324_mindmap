1eb72c8a2a40fbea39d502a4ae638df5ce6cb89d
k-wakamatsu-tms <69659176+k-wakamatsu-tms@users.noreply.github.com>
2023-03-31 19:06:58 +0900

    ip

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,39 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export const config = {
+//   matcher: ["/", "/index"],
+// };
+
+// export function middleware(req: NextRequest) {
+//   const basicAuth = req.headers.get("authorization");
+//   const url = req.nextUrl;
+
+//   if (basicAuth) {
+//     const authValue = basicAuth.split(" ")[1];
+//     const [user, pwd] = atob(authValue).split(":");
+
+//     if (user === process.env.user && pwd === process.env.pwd) {
+//       return NextResponse.next();
+//     }
+//   }
+//   url.pathname = "/api/auth";
+
+//   return NextResponse.rewrite(url);
+// }
+
+// middleware.ts
+import { makeIPInspector } from "next-fortress/ip";
+
+/*
+  type IPs = string | Array<string>
+  type makeIPInspector = (allowedIPs: IPs, fallback: Fallback) => Middleware
+  // IP can be specified in CIDR format. You can also specify multiple IPs in an array.
+*/
+export const middleware = makeIPInspector("153.246.177.76", {
+  type: "redirect",
+  destination: "/error",
+});
 
 export const config = {
-  matcher: ["/", "/index"],
+  matcher: ["/"],
 };
-
-export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get("authorization");
-  const url = req.nextUrl;
-
-  if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1];
-    const [user, pwd] = atob(authValue).split(":");
-
-    if (user === process.env.user && pwd === process.env.pwd) {
-      return NextResponse.next();
-    }
-  }
-  url.pathname = "/api/auth";
-
-  return NextResponse.rewrite(url);
-}
